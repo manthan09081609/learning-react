b1fc6d90ec3296628c35efb7f26ab318e9eb0a30
Manthan Sharma <mayank9870866130@gmail.com>
2024-02-08 15:58:09 +0530

    Creating Own React : Version 1

---
+++ 02_customreact/customreact.js
@@ -0,0 +1,21 @@
+function customRender(element, container) {
+  const domElement = document.createElement(element.type);
+  domElement.innerHTML = element.children;
+  domElement.setAttribute("href", element.props.href);
+  domElement.setAttribute("target", element.props.target);
+
+  container.appendChild(domElement);
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+const mainContainer = document.querySelector("#root");
+
+customRender(reactElement, mainContainer);
